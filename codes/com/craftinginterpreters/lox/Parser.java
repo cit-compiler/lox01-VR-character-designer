fd9aa2cf3194d11bd92bf643ebc5e55021180d32
HK-design <cikz22089@g.nihon-u.ac.jp>
2024-12-24 11:45:17 +0900

    modified Lox Parser GenerateAst

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -12,6 +13,7 @@ class Parser {
   Parser(List<Token> tokens) {
     this.tokens = tokens;
   }
+  /* 
   Expr parse() {
     try {
       return expression();
@@ -19,9 +21,42 @@ class Parser {
       return null;
     }
   }
+  */
+
+  List<Stmt> parse() {
+    List<Stmt> statements = new ArrayList<>();
+    while (!isAtEnd()) {
+      statements.add(statement());
+    }
+
+    return statements; 
+  }
+
+
+
   private Expr expression() {
     return equality();
   }
+
+  private Stmt statement() {
+    if (match(PRINT)) return printStatement();
+
+    return expressionStatement();
+  }
+
+  private Stmt printStatement() {
+    Expr value = expression();
+    consume(SEMICOLON, "Expect ';' after value.");
+    return new Stmt.Print(value);
+  }
+
+  private Stmt expressionStatement() {
+    Expr expr = expression();
+    consume(SEMICOLON, "Expect ';' after expression.");
+    return new Stmt.Expression(expr);
+  }
+
+
   private Expr equality() {
     Expr expr = comparison();
 
